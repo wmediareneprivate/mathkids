812522ebeefc94a7cec4b6e90e0bd011227789ea
rene kühnemann <kuehnemann@ocoder.de>
2019-07-04 21:29:15 +0200

    fallback

--- assets/js/mkjs/Game.class.js
+++ assets/js/mkjs/Game.class.js
@@ -3,9 +3,12 @@ class Game
 
     constructor(gameGenerator, parentElement)
     {
+
         this.gameGenerator = gameGenerator;
 
         this.parentElement = parentElement;
+
+        this.currExercise = null;
 
         this.layout = null;
 
@@ -15,6 +18,8 @@ class Game
 
         this.answerBtb = null;
 
+        this.answerDelBtm = null;
+
         this.init();
     }
 
@@ -23,7 +28,8 @@ class Game
         this.layout = $('<div id="game_layout" class="game_layout"></div>');
         this.exerciseOut = $('<div id="exercise_out" class="exercise_out"></div>');
         this.answerTxt = $('<input id="answer_txt" class="answer_txt"></div>');
-        this.answerBtb = $('<input type="button" id="answer_btn" class="answer_btn" value="check"></div>');
+        this.answerBtb = $('<input type="button" id="answer_btn" class="answer_btn" value="check in"></div>');
+        this.answerDelBtm = $('<input type="button" id="answer_del_btn" class="answer_del_btn" value="x"></div>');
 
         this.setLayout();
         this.setEventListeners();
@@ -35,18 +41,56 @@ class Game
         this.parentElement.append(this.layout);
         this.layout.append(this.exerciseOut);
         this.layout.append(this.answerTxt);
+        this.layout.append(this.answerDelBtm);
         this.layout.append(this.answerBtb);
-
-        this.exerciseOut.html('1 x 1');
 
     }
 
     setEventListeners(){
 
-        this.answerBtb.on('click', function(){
-            alert('you gave an answer');
-        });
+        this.answerDelBtm.on('click', ()=> this.answerTxt.val('') );
 
+        this.answerBtb.on('click', ()=> this.handleAnswer() );
+
+    }
+
+    handleAnswer()
+    {
+
+        var answ = this.answerTxt.val();
+
+        this.currExercise.answer = answ;
+
+        console.log('you gave an answer by method : '+answ );
+        console.log(this.currExercise);
+
+        this.setNextExercise();
+    }
+
+    setNextExercise(){
+
+        this.currExercise = this.gameGenerator.getNext();
+
+        if(this.currExercise === false){
+            this.end();
+            return;
+        }
+
+        this.exerciseOut.html(this.currExercise.presentationString);
+
+        this.answerTxt.val('');
+
+
+    }
+
+    start(){
+        this.setNextExercise();
+    }
+
+    end(){
+        console.log('ended game');
+        console.log(this.gameGenerator);
+        this.answerBtb.remove();
     }
 
 }
--- assets/js/mkjs/Launcher.js
+++ assets/js/mkjs/Launcher.js
@@ -13,30 +13,23 @@ class Launcher
         var testClass = new TestClass("[value from launcher]");
 
         var gameGenMult = new GameGeneratorMulipTable(10);
-/*
-        console.log(gameGenMult);
-        
-        var ex = null;
-
-        while(ex !== false){
-            ex = gameGenMult.getNext();
-            if(ex === false)break;
-            console.log(ex.presentationString);
-        } */
-
         gameGenMult.resetPointer();
 
         var game = new Game(gameGenMult, $('#game_box'));
+        game.start();
 
     }
 
 
 }
 
-$(document).ready(function ()
-{
+/* call launcher by anonymous inline function */
+(
+    function(){
+        $(document).ready(function ()
+        { launcher = new Launcher().start(); });
+    }
 
-    launcher = new Launcher().start();
+)();
 
-});
 
